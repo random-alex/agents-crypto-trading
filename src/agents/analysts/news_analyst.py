b97667ac8d37f39dbddda9237451110566b3b6be
random-alex <aleksei.zotov.ml@gmail.com>
2025-10-09 19:54:37 +0700

    feat: add coingecko MCP to the news agent

--- src/agents/analysts/news_analyst.py
+++ src/agents/analysts/news_analyst.py
@@ -1,20 +1,43 @@
 import logfire
 from pydantic_ai import Agent
-from pydantic_ai.builtin_tools import WebSearchTool, MemoryTool, UrlContextTool
+from pydantic_ai.mcp import MCPServerStreamableHTTP, MCPServerSSE, MCPServerStdio
+from pydantic_ai.builtin_tools import WebSearchTool
 from src.prompts.analysts import SYSTEM_PROMPT_NEWS_ANALYST
 from src.config import Config
 
-logfire.configure()
-logfire.instrument_pydantic_ai()
-logfire.instrument_httpx(capture_all=True)
 
+logfire.configure(service_name="News agent")
+logfire.instrument_pydantic_ai()
+
+# mcp_alpha_vantage = MCPServerStreamableHTTP(
+#     url=f"https://mcp.alphavantage.co/mcp?apikey={Config.ALPHA_VANTAGE_API_KEY}&categories=cryptocurrencies,alpha_intelligence"
+# )
+
+
+mcp_coingecko = MCPServerStdio(
+    "npx",
+    args=["-y", "@coingecko/coingecko-mcp"],
+    env={
+        "COINGECKO_PRO_API_KEY": "",
+        "COINGECKO_DEMO_API_KEY": Config.COINGECKO_API_KEY,
+        "COINGECKO_ENVIRONMENT": "demo",
+    },
+    timeout=600,
+)
 
 agent = Agent(
     model=Config.MODEL_VERSION_NEWS_AGENT,
     instructions=SYSTEM_PROMPT_NEWS_ANALYST,
     builtin_tools=[WebSearchTool()],
+    toolsets=[mcp_coingecko],
 )
 
+
 if __name__ == "__main__":
-    res = agent.run_sync("Give me latest news about Bitcoin")
-    print(res)
+    SYMBOL = "Bitcoin"
+    user_prompt = (
+        f"Give me analysis of the latest news, sentiment and price about {SYMBOL}",
+        "Keep the analysis short and coinces. Your analysis will be used by trader later.",
+    )
+    res = agent.run_sync(user_prompt)
+    print(res.output)
--- src/config.py
+++ src/config.py
@@ -6,7 +6,10 @@ load_dotenv()
 
 class Config:
     # Agent's models set up
-    MODEL_VERSION_NEWS_AGENT: str = "anthropic:claude-3-7-sonnet-latest"
+    MODEL_VERSION_NEWS_AGENT: str = "anthropic:claude-sonnet-4-5"
 
     # Credentials
     ANTHROPIC_API_KEY: str = os.getenv("ANTHROPIC_API_KEY", "")
+    ALPHA_VANTAGE_API_KEY: str = os.getenv("ALPHA_VANTAGE_API_KEY", "")
+    COINGECKO_API_KEY: str = os.getenv("COINGECKO_DEMO_API_KEY", "")
+    COINGECKO_ENVIRONMENT: str = os.getenv("COINGECKO_ENVIRONMENT", "demo")
